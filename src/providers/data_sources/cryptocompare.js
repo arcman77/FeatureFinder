c4c00da1dfb1ac884af8e00885e81a70f5ddd5ae
Kevin Kennis <kevinvkennis@gmail.com>
2017-08-02 11:22:16 -0700

    add socket io and set up socket for cryptocompare

--- src/providers/data_sources/cryptocompare.js
+++ src/providers/data_sources/cryptocompare.js
@@ -1,7 +1,11 @@
+import io from 'socket.io-client';
+
 class CryptoCompare {
-    constructor() {
+    constructor(selectedCoins) {
         this.name = 'cryptocompare';
         this.baseUrl = 'https://www.cryptocompare.com';
+        this.socket = io.connect('https://streamer.cryptocompare.com/');
+        this.selectedCoins = selectedCoins || [];
         this.actions = {
 
         };
